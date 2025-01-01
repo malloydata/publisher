fdd287f3ec46eef3ec63009bfb40326cf61d0566
Jan Guzman <janfrancisco19@gmail.com>
2025-01-01 18:23:31 -0400

    feat: add links to embed entire notebook/model
    Signed-off-by: Jan Guzman <janfrancisco19@gmail.com>

--- packages/sdk/src/components/Package/Package.tsx
+++ packages/sdk/src/components/Package/Package.tsx
@@ -1,10 +1,23 @@
-import { Divider, Grid2, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import {
+   Divider,
+   Grid2,
+   Typography,
+   Stack,
+   CardActions,
+   IconButton,
+   Tooltip,
+   Collapse,
+} from "@mui/material";
+import LinkOutlinedIcon from "@mui/icons-material/LinkOutlined";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { Notebook } from "../Notebook";
 import { StyledCard, StyledCardContent, StyledCardMedia } from "../styles";
 import Config from "./Config";
 import Models from "./Models";
 import Databases from "./Databases";
 import Schedules from "./Schedules";
+import { highlight } from "../highlighter";
 
 const README_NOTEBOOK = "README.malloynb";
 
@@ -23,6 +36,24 @@ export default function Package({
    navigate,
    accessToken,
 }: PackageProps) {
+   const [embeddingExpanded, setEmbeddingExpanded] =
+      React.useState<boolean>(false);
+   const [highlightedEmbedCode, setHighlightedEmbedCode] =
+      React.useState<string>();
+
+   const packageCodeSnippet = getNotebookCodeSnippet(
+      server,
+      packageName,
+      README_NOTEBOOK,
+      true,
+   );
+
+   useEffect(() => {
+      highlight(packageCodeSnippet, "typescript").then((code) => {
+         setHighlightedEmbedCode(code);
+      });
+   }, [embeddingExpanded]);
+
    if (!navigate) {
       navigate = (to: string) => {
          window.location.href = to;
@@ -72,9 +103,76 @@ export default function Package({
          <Grid2 size={{ md: 12 }}>
             <StyledCard variant="outlined">
                <StyledCardContent>
-                  <Typography variant="overline" fontWeight="bold">
-                     Readme
-                  </Typography>
+                  <Stack
+                     sx={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                     }}
+                  >
+                     <Typography variant="overline" fontWeight="bold">
+                        Readme
+                     </Typography>
+                     <CardActions
+                        sx={{
+                           padding: "0px 10px 0px 10px",
+                           mb: "auto",
+                           mt: "auto",
+                        }}
+                     >
+                        <Tooltip
+                           title={
+                              embeddingExpanded
+                                 ? "Hide Embedding"
+                                 : "View Embedding"
+                           }
+                        >
+                           <IconButton
+                              size="small"
+                              onClick={() => {
+                                 setEmbeddingExpanded(!embeddingExpanded);
+                              }}
+                           >
+                              <LinkOutlinedIcon />
+                           </IconButton>
+                        </Tooltip>
+                     </CardActions>
+                  </Stack>
+                  <Collapse in={embeddingExpanded} timeout="auto" unmountOnExit>
+                     <Divider />
+                     <Stack
+                        sx={{
+                           p: "10px",
+                           borderRadius: 0,
+                           flexDirection: "row",
+                           justifyContent: "space-between",
+                        }}
+                     >
+                        <Typography
+                           sx={{
+                              fontSize: "12px",
+                              "& .line": { textWrap: "wrap" },
+                           }}
+                        >
+                           <div
+                              dangerouslySetInnerHTML={{
+                                 __html: highlightedEmbedCode,
+                              }}
+                           />
+                        </Typography>
+                        <Tooltip title="Copy Embeddable Code">
+                           <IconButton
+                              sx={{ width: "24px", height: "24px" }}
+                              onClick={() => {
+                                 navigator.clipboard.writeText(
+                                    packageCodeSnippet,
+                                 );
+                              }}
+                           >
+                              <ContentCopyIcon />
+                           </IconButton>
+                        </Tooltip>
+                     </Stack>
+                  </Collapse>
                   <Divider />
                </StyledCardContent>
                <StyledCardMedia>
@@ -91,4 +189,21 @@ export default function Package({
          </Grid2>
       </Grid2>
    );
+}
+
+function getNotebookCodeSnippet(
+   server: string,
+   packageName: string,
+   notebookPath: string,
+   expandedCodeCells: boolean,
+): string {
+   return `
+<Notebook
+   server="${server}"
+   packageName="${packageName}"
+   notebookPath="${notebookPath}"
+   versionId={versionId}
+   accessToken={accessToken}
+   expandCodeCells={${expandedCodeCells}}
+/>`;
 }
