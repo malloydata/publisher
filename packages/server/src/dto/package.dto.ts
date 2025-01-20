60fee3c11bf40739d0163784b4636cf8ca5983a2
Jan Guzman <janfrancisco19@gmail.com>
2025-01-20 10:28:17 -0400

    reference generated code from api definition
    Signed-off-by: Jan Guzman <janfrancisco19@gmail.com>

--- packages/server/src/dto/package.dto.ts
+++ packages/server/src/dto/package.dto.ts
@@ -1,6 +1,7 @@
 import { IsString, IsNotEmpty } from "class-validator";
+import { ApiPackage } from "../service/package";
 
-export class PackageDto {
+export class PackageDto implements ApiPackage {
    @IsString()
    @IsNotEmpty()
    name: string;
