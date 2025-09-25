3faed145af6769a729eafb61e61b5b5eca236eac
manojdeep14 <manojdeep14d@gmail.com>
2025-09-25 14:08:29 -0600

    Added missing connectString parameter in getPostgresConnection
    Signed-off-by: manojdeep14 <manojdeep14d@gmail.com>

--- packages/server/src/service/db_utils.ts
+++ packages/server/src/service/db_utils.ts
@@ -26,6 +26,7 @@ async function getPostgresConnection(
       database: apiPostgresConnection.databaseName,
       password: apiPostgresConnection.password,
       port: apiPostgresConnection.port,
+      connectionString: apiPostgresConnection.connectionString,
       max: 10,
       idleTimeoutMillis: 30000,
    });
@@ -127,6 +128,7 @@ export async function getSchemasForConnection(
          throw new Error("Postgres connection is required");
       }
       const pool = await getPostgresConnection(connection.postgresConnection);
+      console.log("pool", pool);
       const res = await pool.query(
          "SELECT schema_name FROM information_schema.schemata",
       );
