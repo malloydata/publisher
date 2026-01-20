674489781759a1bbac21a39be84de36aca01134a
Josh Sacks <josh.sacks@gmail.com>
2026-01-20 13:18:02 -0700

    use retrieval function in README in package
    Signed-off-by: Josh Sacks <josh.sacks@gmail.com>

--- packages/sdk/src/components/Package/Package.tsx
+++ packages/sdk/src/components/Package/Package.tsx
@@ -1,4 +1,5 @@
 import { Box, Grid } from "@mui/material";
+import React from "react";
 import { encodeResourceUri, parseResourceUri } from "../../utils/formatting";
 import { RetrievalFunction } from "../filter/DimensionFilter";
 import { Notebook } from "../Notebook";
