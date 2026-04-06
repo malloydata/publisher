1ae0a4a64656a1ee272e93cbce3a2ddd79be66b4
Michael Wu <michael@credibledata.com>
2026-04-06 12:50:37 -0600

    Remove dead type reference

--- packages/server/src/controller/model.controller.ts
+++ packages/server/src/controller/model.controller.ts
@@ -6,8 +6,6 @@ type ApiNotebook = components["schemas"]["Notebook"];
 type ApiModel = components["schemas"]["Model"];
 type ApiCompiledModel = components["schemas"]["CompiledModel"];
 type ApiRawNotebook = components["schemas"]["RawNotebook"];
-export type ListModelsFilterEnum =
-   components["parameters"]["ListModelsFilterEnum"];
 export class ModelController {
    private projectStore: ProjectStore;
 
